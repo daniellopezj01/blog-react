30a6caf116d0a0865105501958b742471794a5d5
daniel hizo un comentario <daniellopezj0327@gmail.com>
2020-05-12 18:09:29 -0500

    cambios

--- src/index.js
+++ src/index.js
@@ -7,11 +7,6 @@ import {createStore, applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
 import reduxThunk from 'redux-thunk'
 
-
-
-
-
-/**pruebaaaaa */
 import reducers from "./reducer";
 const store = createStore(
   reducers,//todos los reducer
