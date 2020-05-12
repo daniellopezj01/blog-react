ac141f23cf31cb24a555e7ee545cc983d7850fd9
daniel hizo un comentario <daniellopezj0327@gmail.com>
2020-05-12 18:44:47 -0500

    add in master

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import {createStore, applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
 import reduxThunk from 'redux-thunk'
 
-/**pruebaaaaa */
 import reducers from "./reducer";
 const store = createStore(
   reducers,//todos los reducer
